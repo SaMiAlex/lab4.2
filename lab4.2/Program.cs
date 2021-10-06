e48894a94fd6f4f832dee354a4e8fcc0589173f6
SaMiAlex <samias@mail.ru>
2021-10-06 10:42:08 +0600

    Initial commit

---
+++ lab4.2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательность чисел");
+            int p = 0;
+            int n;
+
+            do
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n > 0)
+                {
+                    p++;
+                }
+                if (n < 0)
+                {
+                    p--;
+                }
+
+            } while (n != 0);
+
+            if (p > 0)
+            {
+                Console.WriteLine("Положительных больше");
+            }
+            if (p < 0)
+            {
+                Console.WriteLine("Отрицательных больше");
+            }
+            if (p == 0)
+            {
+                Console.WriteLine("Поровну");
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
